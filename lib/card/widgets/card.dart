c78072669827d12041591ef7f3c78ab317baf5d6
Leonardo Sallustio <117084194+LeoSallu@users.noreply.github.com>
2024-10-17 11:26:42 +0200

    :sparkles: feat: add flip animation to card

--- lib/card/widgets/card.dart
+++ lib/card/widgets/card.dart
@@ -1,5 +1,6 @@
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class FidelityCard extends StatefulWidget {
   const FidelityCard({super.key});
@@ -35,14 +36,45 @@ class _FidelityCardState extends State<FidelityCard> {
             alignment: Alignment.centerRight,
             child: _turnCardButton(context),
           ),
-          isFront ? _buildFrontCard(context) : _buildBackCard(context),
+          _buildFlipAnimation(context),
         ],
       ),
     );
   }
 
+  AnimatedSwitcher _buildFlipAnimation(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        final rotationAnimation =
+            Tween(begin: math.pi, end: 0.0).animate(animation);
+        return AnimatedBuilder(
+          animation: rotationAnimation,
+          child: child,
+          builder: (context, child) {
+            final isBackVisible = ValueKey(isFront) != child!.key;
+            var tilt = (animation.value - 0.5).abs() - 0.5;
+            tilt *= isBackVisible ? -0.003 : 0.003;
+
+            final rotationValue = isBackVisible
+                ? math.min(rotationAnimation.value, math.pi / 2)
+                : rotationAnimation.value;
+
+            return Transform(
+              transform: Matrix4.rotationY(rotationValue)..setEntry(3, 0, tilt),
+              alignment: Alignment.center,
+              child: child,
+            );
+          },
+        );
+      },
+      child: isFront ? _buildFrontCard(context) : _buildBackCard(context),
+    );
+  }
+
   Column _buildFrontCard(BuildContext context) {
     return Column(
+      key: const ValueKey('front'),
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -92,6 +124,7 @@ class _FidelityCardState extends State<FidelityCard> {
 
   Column _buildBackCard(BuildContext context) {
     return Column(
+      key: const ValueKey('back'),
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -117,6 +150,7 @@ class _FidelityCardState extends State<FidelityCard> {
             width: double.infinity,
             height: 110,
             data: '0401021022323',
+            color: Theme.of(context).colorScheme.onSecondaryFixed,
           ),
         ),
       ],
